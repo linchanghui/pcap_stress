d65b086b6f68f6325dd05539669bcecfcd3e0cc0
linchanghui <798249354@qq.com>
2016-11-18 15:03:38 +0800

    first commit

---
+++ main.c
@@ -0,0 +1,11 @@
+//
+// Created by linchanghui on 11/18/16.
+//
+
+#include "main.h"
+#include <stdlib.h>
+
+main (int argc,char *argv[]) {
+
+
+}
---
+++ main.h
@@ -0,0 +1,8 @@
+//
+// Created by linchanghui on 11/18/16.
+//
+
+#ifndef PACP_STRESS_MAIN_H
+#define PACP_STRESS_MAIN_H
+
+#endif //PACP_STRESS_MAIN_H
